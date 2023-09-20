f8b49c73d9e5341c5980a4bf37e6cbd9adf69dd1
nayoung03 <naje311@naver.com>
2023-09-20 14:51:20 +0900

    Initial commit

---
+++ src/main/java/com/example/board20231/Board20231Application.java
@@ -0,0 +1,13 @@
+package com.example.board20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board20231Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Board20231Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/board20231/Board20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board20231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board20231ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
