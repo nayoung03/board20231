dfa0528d1a884d08b8d47e3e4ef3d8f68377f0c4
nayoung03 <naje311@naver.com>
2023-09-21 17:06:12 +0900

    답변등록 추가

--- src/main/java/com/example/board20231/question/QuestionService.java
+++ src/main/java/com/example/board20231/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.example.board20231.question;
 
+import com.example.board20231.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,9 +15,17 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public List<Question> getList() {
-
         return this.questionRepository.findAll();
-
     }
 
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+
+
+    }
 }
