78bb707c14ae16eba6f8645c9a60b3447baf32c0
nayoung03 <naje311@naver.com>
2023-09-21 14:55:01 +0900

    서비스 추가

---
+++ src/main/java/com/example/board20231/question/QuestionService.java
@@ -0,0 +1,20 @@
+package com.example.board20231.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+
+        return this.questionRepository.findAll();
+
+    }
+
+}
